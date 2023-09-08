52cd45fd0dab9d7bea215f74d72f5265cbbf1abb
inathiB <108303226+inathiB@users.noreply.github.com>
2023-09-08 10:08:44 +0200

    Create ApplicationCreation.java

---
+++ application/src/main/java/com/example/demo/ApplicationCreation.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@Data
+@Document
+public class ApplicationCreation {
+    @Id
+    private String id;
+    @Indexed(unique = true)
+    private String department;
+    private String details;   
+    private LocalDateTime duedate;
+
+    public Application(String department, String details, LocalDateTime duedate) {
+        this.department = department;
+        this.details = details;
+        this.duedate = duedate;
+    }
+}
