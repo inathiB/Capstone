ee81e8a43c1fdcf0fcc6cabac685b8c8e67f1df1
inathiB <108303226+inathiB@users.noreply.github.com>
2023-09-14 16:35:46 +0200

    Update Tutorial.java

--- demo/src/main/java/com/example/demo/Collection/Tutorial.java
+++ demo/src/main/java/com/example/demo/Collection/Tutorial.java
@@ -17,15 +17,17 @@ public class Tutorial {
     @Id
     private String tutorialID;
     private String courseCode;
+    private String title
     private DayOfWeek dayOfWeek;
     private LocalTime time;
     private List<Tutor> tutTutors;
     private int numberOfTutors;
     private String venue;
 
-    public Tutorial(String tutorialID, String courseCode, DayOfWeek dayOfWeek, LocalTime time, int numberOfTutors) {
+    public Tutorial(String tutorialID, String courseCode,String title, DayOfWeek dayOfWeek, LocalTime time, int numberOfTutors) {
         this.tutorialID = tutorialID;
         this.courseCode = courseCode;
+        this.title = title;
         this.dayOfWeek = dayOfWeek;
         this.time = time;
         this.numberOfTutors = numberOfTutors;
